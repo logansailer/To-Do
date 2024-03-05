86ea09e82e290231c227784afbcfaadd44a83609
Logan <logansailer0633@gamil.com>
2024-03-05 20:40:37 +0900

    adds logic to add to olist

---
+++ index.js
@@ -0,0 +1,55 @@
+//selects elements from DOM
+const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
+
+//saves elements upon page refresh into JSON, otherwise from empty array
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+let lists = JSON.parse(localStorage.setItem(LOCAL_STORAGE_LIST_KEY)) || [];
+
+//
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.value
+    if(listName == null || listName ==='') return;
+    const list = createList(listName)
+    newListInput.value = null;
+    lists.push(list);
+    saveAndRender();
+})
+
+//creates a list list
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+//saves list element to local storage
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+}
+
+function saveAndRender() {
+    save();
+    render();
+}
+
+//renders new list to DOM
+function render() {
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
+        listElement.classList.add("list-name");
+        listElement.innerText = list.name;
+        listsContainer.appendChild(listElement);
+    })
+}
+
+//clears list element from DOM
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+render();
